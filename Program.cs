92847f4ab6a53f7d0d7dce22dae5b9796f99649c
Brandon894 <51420356+Brandon894@users.noreply.github.com>
2019-06-05 23:09:35 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejerciciono._1Impresiondenumerosdel0al100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+            while (n <= 100)
+            {
+                Console.WriteLine(n);
+                n = n + 1;
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
